4a1c96374ee0964dee15728da8963a9a5bf36ea3
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-09-04 17:06:02 +0500

    feat: тест по фильтрации (с включенным фильтром)

--- spec/integration/Filter.spec.tsx
+++ spec/integration/Filter.spec.tsx
@@ -1,7 +1,47 @@
+import ue from '@testing-library/user-event'
+import { App } from '../../src/App'
+import { List } from '../../src/components/List'
+
+import { render, screen } from '@testing-library/react'
+import { Task } from 'src/utils/types'
+
+const userEvent = ue.setup({
+	advanceTimers: jest.advanceTimersByTime
+})
+
 describe('Список задач', () => {
-	// не содержит выполненные задачи
-	// после нажатия на кнопку фильтрации
-	it.todo('с включенным фильтром')
+	it('С включенным фильтром', async () => {
+		const onDelete = jest.fn()
+		const onToggle = jest.fn()
+
+		const items: Task[] = [
+			{ id: '325235', header: 'убраться дома', done: false },
+			{ id: '22377', header: 'приготовить ужин', done: false },
+			{ id: '42', header: 'выполнить дз', done: false }
+		]
+
+		render(<App />)
+		const { rerender, asFragment } = render(
+			<List items={items} onDelete={onDelete} onToggle={onToggle} />
+		)
+
+		const screenFirstRender = asFragment()
+
+		await userEvent.click(screen.getByText(/Невыполненные задачи/i))
+
+		expect(screen.queryByText('доделать отчет по работе')).toBeNull()
+
+		rerender(<List items={items} onDelete={onDelete} onToggle={onToggle} />)
+
+		const screenSecondRender = asFragment()
+
+		expect(screen.queryByText('доделать отчет по работе')).toBeNull()
+		expect(screen.getByText('убраться дома')).toBeInTheDocument()
+		expect(screen.getByText('выполнить дз')).toBeInTheDocument()
+		expect(screen.getByText('приготовить ужин')).toBeInTheDocument()
+
+		expect(screenFirstRender).toMatchDiffSnapshot(screenSecondRender)
+	})
 
 	// показывает как выполненные, так и не выполненные задачи
 	// после повторного нажатия на кнопку фильтрации
