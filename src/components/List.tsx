a0c2e9eda75225e577c8aee1ee4842d59a8bbf79
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-09-04 17:04:04 +0500

    feat: функционал фильтрации

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,19 +1,43 @@
-import { Item } from "./Item";
+import { useState, useMemo } from 'react'
+import { Item } from './Item'
+import { Task } from 'src/utils/types'
 
 type Props = {
-  items: Task[];
-  onDelete: (id: Task["id"]) => void;
-  onToggle: (id: Task["id"]) => void;
-};
+	items: Task[]
+	onDelete: (id: Task['id']) => void
+	onToggle: (id: Task['id']) => void
+}
 
-export const List = ({ items, onDelete, onToggle }: Props) => (
-  <ul className="task-list tasks">
-    {items.map((item) => (
-      <Item
-        {...item}
-        key={item.id}
-        onDelete={onDelete}
-        onToggle={onToggle} />
-    ))}
-  </ul>
-);
+export const List = ({ items, onDelete, onToggle }: Props) => {
+	const [filter, setFilter] = useState<boolean>(false)
+
+	const filteredItems = useMemo(() => {
+		return filter ? items.filter(item => !item.done) : items
+	}, [filter, items])
+
+	const toggleFilter = () => setFilter(prevFilter => !prevFilter)
+
+	const getButtonText = () => {
+		return filter ? 'Все задачи' : 'Невыполненные задачи'
+	}
+
+	const buttonClass = `button ${filter ? 'button-all' : 'button-active'}`
+
+	return (
+		<>
+			<button onClick={toggleFilter} className={buttonClass}>
+				{getButtonText()}
+			</button>
+			<ul className='task-list tasks' data-testid='task-list'>
+				{filteredItems.map(item => (
+					<Item
+						{...item}
+						key={item.id}
+						onDelete={onDelete}
+						onToggle={onToggle}
+					/>
+				))}
+			</ul>
+		</>
+	)
+}
