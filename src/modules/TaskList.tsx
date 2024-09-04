6aa80325fe74125f0efbc7f81a690f765842f6dd
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-09-04 17:04:40 +0500

    chore: импорт типа Задачи

--- src/modules/TaskList.tsx
+++ src/modules/TaskList.tsx
@@ -1,23 +1,24 @@
-import { useDispatch, useSelector } from "react-redux";
-import { Empty } from "src/components/Empty";
-import { List } from "src/components/List";
-import { deleteTask, tasksSelector, toggleTask } from "src/store/taskSlice";
+import { useDispatch, useSelector } from 'react-redux'
+import { Empty } from 'src/components/Empty'
+import { List } from 'src/components/List'
+import { deleteTask, tasksSelector, toggleTask } from 'src/store/taskSlice'
+import { Task } from 'src/utils/types'
 
 export const TaskList = () => {
-  const items = useSelector(tasksSelector);
-  const dispatch = useDispatch();
+	const items = useSelector(tasksSelector)
+	const dispatch = useDispatch()
 
-  const handleDelete = (id: Task["id"]) => {
-    dispatch(deleteTask(id));
-  };
+	const handleDelete = (id: Task['id']) => {
+		dispatch(deleteTask(id))
+	}
 
-  const handleToggle = (id: Task["id"]) => {
-    dispatch(toggleTask(id));
-  };
+	const handleToggle = (id: Task['id']) => {
+		dispatch(toggleTask(id))
+	}
 
-  return items.length > 0 ? (
-    <List items={items} onDelete={handleDelete} onToggle={handleToggle} />
-  ) : (
-    <Empty />
-  );
-};
+	return items.length > 0 ? (
+		<List items={items} onDelete={handleDelete} onToggle={handleToggle} />
+	) : (
+		<Empty />
+	)
+}
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,5 +1,5 @@
-type Task = {
-    id: string;
-    header: string;
-    done: boolean;
+export type Task = {
+	id: string
+	header: string
+	done: boolean
 }
