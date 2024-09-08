b743661379284c90b38eb7500c8681c82ffbbe77
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-09-08 10:57:03 +0500

    feat: unit test Item.spec.tsx- не больше 32 символов

--- spec/unit/Item.spec.tsx
+++ spec/unit/Item.spec.tsx
@@ -1,5 +1,16 @@
+import { render, screen } from '@testing-library/react'
+import { Item } from 'src/components/Item'
+import { Task } from 'src/utils/types'
 describe('Элемент списка задач', () => {
-    it.todo('название не должно быть больше 32 символов');
-    it.todo('название не должно быть пустым');
-    it.todo('нельзя удалять невыполненные задачи');
-});
+	const onDelete = jest.fn()
+	const onToggle = jest.fn()
+
+	const task = { id: '42', header: 'Приготовить ужин', done: true }
+
+	it('название не должно быть больше 32 символов', () => {
+		render(<Item {...task} onDelete={onDelete} onToggle={onToggle} />)
+		expect(screen.getByText(task.header)).toHaveTextContent(/^.{0,32}$/)
+	})
+	it.todo('название не должно быть пустым')
+	it.todo('нельзя удалять невыполненные задачи')
+})
