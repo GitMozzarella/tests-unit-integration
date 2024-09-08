c340f3589cfc934c21f562e30997373cf8577800
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-09-08 11:52:35 +0500

    feat: intergration Notifier.spec.tsx - появляется уведомление с выполненной таской и одновременно только одно уведомление

--- spec/integration/Notifier.spec.tsx
+++ spec/integration/Notifier.spec.tsx
@@ -1,7 +1,20 @@
-import { render, screen } from "@testing-library/react";
-import { Notifier } from "src/components/Notifier";
+import { render, screen } from '@testing-library/react'
+import { Notifier } from 'src/components/Notifier'
 
-describe('Оповещение при вополнении задачи', () => {
-    it.todo('появляется и содержит заголовок задачи');
-    it.todo('одновременно может отображаться только одно');
-});
+describe('Оповещение при выполнении задачи', () => {
+	it('при выполнении задачи должно появляться оповещение', () => {
+		const onClose = jest.fn()
+		render(<Notifier task='Прочитать ТЗ' open={true} onClose={onClose} />)
+		expect(screen.getByText('Прочитать ТЗ')).toBeInTheDocument()
+	})
+
+	it('одновременно может быть только одно оповещение', () => {
+		const onClose = jest.fn()
+		const { rerender } = render(
+			<Notifier task='Прочитать ТЗ' open={true} onClose={onClose} />
+		)
+		rerender(<Notifier task='Приготовить ужин' open={true} onClose={onClose} />)
+		expect(screen.getByText('Приготовить ужин')).toBeInTheDocument()
+		expect(screen.queryByText('Прочитать ТЗ')).not.toBeInTheDocument()
+	})
+})
